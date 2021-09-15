f98e2521f49c7e3c5e91fd185df4b36bb2f0f178
john <83730668+DevJohn2021@users.noreply.github.com>
2021-09-15 17:28:50 -0500

    cf pausado tm3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -28,62 +28,179 @@
     .row.justify-content-center
       .col-lg-5
         .bloque-texto-b.color-primario.p-4
-          .bloque-texto-b__texto
-            //i.fas.fa-quote-left
-            p.mb-0 pero,
-            figure.px-2
-              img.mx-auto(src='@/assets/curso/tema2/question-open.svg', alt='imagen ambiental', style='width:27px;')
-            h4.mb-0 cómo definir cuál es la alternativa que mejor se adecúa a la empresa
-            i.fas.fa-question
+          .bloque-texto-b__texto 
+            h4.mb-0 Pero, ¿cómo definir cuál es la alternativa que mejor se adecúa a la empresa?
+            
 
     p.mb-4 
     .row
       .col-lg-12
-      .titulo-sexto.color-primario
-        p.mb-0 Tabla 1 Concentración y diversificación
-    .row
-      .col-lg-12
+        .titulo-sexto.color-primario
+          p.mb-0 Tabla 1 Concentración y diversificación
+    .row.mb-5
+      .col-lg-12.mb-5
         .tabla-b.color-acento-contenido.mb-5
           table
-            caption Leyenda de la tabla
+            caption Nota. Tomada de Llamazares (2017).
             thead
               tr
                 th.text-white.text-center(style='width:50%; background-color: #FD8F25; font-size:18px; border-right:0;')  CONCENTRACIÓN 
                 th.text-white.text-center(style='background-color: #FD8F25; font-size:18px; border-right:0; border-left:0;') DIVERSIFICACIÓN 
             tbody
               tr
-                th(style='width:50%') Vulgar
-                td Get back to work. Learn from criticism. Creativity is a work-ethic. Design as if your life depended on it. Keep going. It isn’t what you are.
+                td(style='width:50%')  El número potencial de clientes en cada mercado es alto. 
+                td  El número potencial de clientes en cada mercado es reducido.
+
               tr
-                th Vulgar
-                td Get back to work. Learn from criticism. Creativity is a work-ethic. Design as if your life depended on it. Keep going. It isn’t what you are.
+                td Los mercados “clave” son estables con una demanda sostenida.
+                td Los mercados “clave” son muy cíclicos con una demanda cambiante.
+      
+              tr
+                td Mercados compartidos por un número amplio de competidores.
+                td Mercados dominados por un número reducido de competidores.
+              tr
+                td La venta se basa en factores distintos al precio.
+                td La venta se realiza fundamentalmente por precio.
+      
+              tr
+                td El ciclo del producto es largo.
+                td El ciclo del producto es corto. 
+              tr
+                td El comprador es fiel al proveedor habitual.
+                td El comprador no es fiel al proveedor habitual.
+      
+              tr
+                td El producto precisa de una adaptación compleja y costosa.
+                td El producto es estándar en los diferentes mercados.
+              tr
+                td La venta exige inversiones en promoción/publicidad.
+                td La venta no exige inversiones en promoción/publicidad.
+      
+              tr
+                td Los costes de administración y seguimiento de las ventas son elevados.
+                td Los costes de administración y seguimiento de las ventas son reducidos.
+              tr
+                td El producto es de venta repetitiva al cliente final.
+                td El producto no es de venta repetitiva al cliente final.
       
 
 
-
-
-
-
-
-
-
     #t_2_1.titulo-segundo
-      .h2 2.1 Infografía Animada
+      .h2 2.1 Concentración/diversificación de mercados
     
-    #t_2_2.titulo-segundo
-      .h2 2.2 Video Motion
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-a.color-primario.p-4.p-md-4.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img.mx-auto(src='@/assets/curso/tema2/img2.svg', alt='imagen ambiental', style='width:224px;')
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p La concentración de mercados se refiere al grado en el que un mercado está aglutinado o consolidado en un número determinado de agentes, sean estos productores o vendedores. De esta forma, un mercado estará más concentrado mientras menor sea el número de empresas y mientras mayor diferencia exista entre el tamaño de estas.
+                  br
+                  br
+                  | (Economipedia, 2021)
+              
+    .tarjeta--container.row.mb-4
+      .col-md.tarjeta.color-secundario.p-5
+        .row.justify-content-center.mb-5
+          .col-auto
+            figure
+              img(src='@/assets/curso/tema2/img3.svg', alt='icono que acompaña', style='width:190px;')
+        p.text-center Las estrategias de diversificación también pueden emplearse cuando la empresa precisa reestructurar alguna línea de negocio. Diversificar consiste en crear nuevos productos o servicios, desarrollar un nuevo enfoque para los productos o servicios existentes, o buscar otros perfiles de consumidores con necesidades o deseos que atender. En esta última opción, analizar los nuevos estilos de vida que van surgiendo, sobre todo generacionalmente, puede aportar una interesante nueva visión a la empresa desde el punto de vista estratégico” (Torreblanca, 2020).
+      .col-md.tarjeta.color-secundario.p-5(style='background-color: #7A58F8')
+        .row.justify-content-center.mb-5
+          .col-auto
+            figure
+              img(src='@/assets/curso/tema2/img4.svg', alt='icono que acompaña', style='width:190px;')
+        p.text-center Por el contrario, la diversificación es definida como una estrategia empresarial que consiste en ampliar el mercado potencial con el objetivo de expandirse y alcanzar un mayor crecimiento, tratando de descubrir nuevos nichos de mercado en los que la empresa pueda penetrar y lograr el éxito.
 
+
+    separador
+    #t_2_2.titulo-segundo
+      .h2 2.2 Agrupación de países 
+
+    p.mb-4
+
+    .row.justify-content-center.mb-4
+      .col-lg-8
+        .titulo-sexto.color-primario
+          p.mb-0 Criterios de selección de mercados
+    .row.justify-content-center.mb-5
+      .col-lg-9.mb-5
+        figure
+          img.mx-auto(src='@/assets/curso/tema2/img5.svg', alt='Criterios de selección de mercados', style='')
+
+    p.mb-5 Luego de esta preselección, y con el grupo más reducido de mercados potenciales, se selecciona el país o ciudad que encaje con los objetivos de la organización.
+
+    .row.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-acento-contenido.p-4.p-md-4.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure.py-3
+                img.mx-auto(src='@/assets/curso/tema2/img6.svg', alt='imagen ambiental', style='width: 260px;')
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p Finalmente, es necesario realizar una definición clara del perfil del mercado al cual se pretende dirigir. Para ello, las compañías y sus equipos de mercadotecnia se enfocan en establecer su mercado objetivo, definiendo cada una de las características deseables en este, con el fin de poner en marcha estrategias que permitan satisfacer sus necesidades, el uso adecuado de los recursos de la empresa y el posicionamiento en el mercado. Este proceso se denomina #[strong segmentación de mercados,] concepto que se ampliará en el siguiente apartado.
+
+
+    separador
     #t_2_3.titulo-segundo
       .h2 2.3 Motion + Presentador
 
-    #t_2_4.titulo-segundo
-      .h2 2.4 Animación 2D
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-b.color-secundario.p-4
+          .bloque-texto-b__texto
+            i.fas.fa-quote-left
+            p.mb-0 Es una de las herramientas de mercadotecnia que permite realizar un análisis de mercado en forma efectiva. Asimismo, puede definirse como la división de un universo heterogéneo en grupos con al menos una característica homogénea. De esta forma, es posible ejempliﬁcar el concepto de segmentación tomando como universo a los habitantes de un país determinado; a partir de ese universo, podrán establecerse subgrupos de acuerdo con características específicas, por ejemplo: mujeres argentinas que viven en ciudades grandes y que tienen entre 15 y 49 años. Este subgrupo, que pertenece al universo de habitantes de Argentina, es un segmento de mercado.
+            i.fas.fa-quote-right
+          .bloque-texto-d__autor 
+            .h5.mb-0 (Fernández, 2009)
 
-    #t_2_5.titulo-segundo
-      .h2 2.5 Video interactivo
+    p.mb-5 Dentro de sus principales ventajas, se encuentran:
 
-    #t_2_6.titulo-segundo
-      .h2 2.6 Infografías
+    .row.justify-content-center
+      .col-lg-10
+        .titulo-sexto.color-primario
+          p.mb-0 Ventajas de la segmentación de mercados
+        
+        figure.mb-5
+          img.mb-4.mx-auto(src='@/assets/curso/tema2/img7.svg', alt='Ventajas de la segmentación de mercados', style='')
+          figcaption Nota. Tomada de Fernández (2009).
+
+    h4.mb-5 Variables de segmentación de mercados 
+    p.mb-4 Es vital reconocer las variables de segmentación de mercados, debido a que brindarán la posibilidad de establecer el grupo de mercado objetivo de manera clara y precisa. Las variables de segmentación del mercado son:
+
+    .row.justify-content-center
+      .col-lg-10
+        .titulo-sexto.color-primario
+          p.mb-0 Variables de segmentación de mercados
+
+    .row.justify-content-center
+      .col-xl-12
+        .row.justify-content-center
+          .col-lg-6
+            figure.mb-5
+              img.mb-4.mx-auto(src='@/assets/curso/tema2/conceptual1.svg', alt='Variables demográficas', style='width: 545px;')
+          .col-lg-6
+            figure.mb-5
+              img.mb-4.mx-auto(src='@/assets/curso/tema2/conceptual2.svg', alt='Variables geográficas', style='width: 545px;')
+          .col-lg-6
+            figure.mb-5
+              img.mb-4.mx-auto(src='@/assets/curso/tema2/conceptual3.svg', alt='Variables psicográficas', style='width: 545px;')
+          .col-lg-6
+            figure.mb-5
+              img.mb-4.mx-auto(src='@/assets/curso/tema2/conceptual4.svg', alt='Variables de posición del usuario o de uso', style='width: 545px;')
+          
+      figcaption Nota. Tomada de Fernández (2009).
+
+
+
+
+    
 
     
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,7 +3,80 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
+    .titulo-principal
+      .titulo-principal__numero
+        span 3
+      h1 Formas de entrada a mercados internacionales
+
     
+
+    p.mb-4 Previamente, se han identificado las pautas para la preselección y selección de mercados, al igual que la segmentación de este, para lograr establecer el mercado disponible para la empresa, es decir, el grupo de personas que estarían dispuestas a adquirir los productos y servicios ofertados por la misma.
+    p.mb-4 Es el momento de determinar la forma de entrada al mercado seleccionado. Esta elección faculta a la empresa para el planteamiento del plan de acción a poner en marcha para la comercialización internacional. Las formas de entrada a mercados son:
+
+
+    .row.mb-5
+      .col-lg-12(style='background-color: #F3F9FF')
+        SlyderC.mb-0(:datos="datosSlyder")
+    
+    
+    p.mb-4 En este orden de ideas, y con el fin de continuar con el proceso de expansión de mercados, llega el momento en el cual las empresas centran su atención en la mezcla de mercado, término utilizado para definir las políticas de producto, precio, plaza y promoción. 
+
+
+
+
+    #t_3_1.titulo-segundo
+      .h2 3.1 Producto
+    
+    p.mb-4 El producto es el bien o tangible que se desea comercializar en un mercado determinado. A nivel internacional, es importante identificar, de la gama de productos de una empresa, cuál o cuáles productos pueden ofrecer mayor rentabilidad y mayor potencial de ventas en el exterior. 
+    p.mb-4 Dentro de la política del producto, hay que definir cada uno de sus atributos y las adaptaciones necesarias para su comercialización en mercados exteriores, teniendo en cuenta que las tendencias de consumo varían de un país a otro.
+    p.mb-4 Los siguientes aspectos son los recomendados a tener en cuenta en la adaptación de los productos: 
+    
+    .row.justify-content-center
+      .col-lg-4
+        figure
+          img.mx-auto(src='@/assets/curso/tema3/img4.svg', alt='imagen ambiental', style='width:420px;')
+            
+      .col-lg-8
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/tema3/icp1.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--azul(style='background-color: #E9E3FF')
+            .p-4
+              p.mb-0 Normativa legal (materias primas, análisis químicos, medioambiente, certificaciones, entre otras) y necesidades de lo clientes (rendimientos, prestaciones, gustos).
+    
+    
+    
+    
+    separador
+    #t_3_2.titulo-segundo
+      .h2 3.2
+    
+    
+    
+    
+    
+    
+    
+    separador
+    #t_3_3.titulo-segundo
+      .h2 3.3
+    
+    
+    
+    
+    
+    
+    
+    separador
+    #t_3_4.titulo-segundo
+      .h2 3.4
+
+
+
+
+
+
+
 
 </template>
 
@@ -12,6 +85,19 @@ export default {
   name: 'Tema3',
   data: () => ({
     // variables de vue
+    datosSlyder: [
+      {
+        imagen: require('@/assets/curso/tema3/img1.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        imagen: require('@/assets/curso/tema3/img2.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        imagen: require('@/assets/curso/tema3/img3.svg'),
+      },
+    ],
   }),
   mounted() {
     this.$nextTick(() => {
